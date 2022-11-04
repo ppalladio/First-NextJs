3490d9e199d43381c7cc01b50461fec1e7bdeeb4
ppalladio <nine.slide@gmail.com>
2022-11-04 14:44:09 +0100

    links between pages with Link

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,20 @@
 // domain/news
-
+import Link from 'next/link';
 function NewsPage() {
-    return <h1> newspage</h1>;
+    return (
+        <>
+            <h1>The News Page</h1>
+            <ul>
+                <li>
+                    <Link href="/news/news1">news item 1</Link>
+                    {/* //'use Link to prevent browser default to send http request, still a SPA  */}
+                </li>
+                <li>
+                    <Link href="/news/news2">news item 2</Link>
+                </li>
+            </ul>
+        </>
+    );
 }
 
 export default NewsPage;
